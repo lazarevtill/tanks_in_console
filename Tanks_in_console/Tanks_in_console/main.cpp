e62377d2f44fa57fb04d0f402f48be3f67504ce9
lazarevtill <lazarevl2001@gmail.com>
2019-11-19 00:55:14 +0300

    Add files via upload

--- Tanks_in_console/Tanks_in_console/main.cpp
+++ Tanks_in_console/Tanks_in_console/main.cpp
@@ -26,8 +26,78 @@ void cleanMap();
 
 void printFire();
 
+const int BLUE = 1;
+const int GREEN = 2;
+const int RED = 4;
+const int WHITE = 15;
+
+void clearScreen(int characterLength) {
+	for (int i = 0; i < characterLength; i++) {
+		cout << "\b";
+	}
+}
+
+void changeColour(int colour) {
+	HANDLE hConsole;
+
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, colour);
+}
+
+void showLoadingScreen() {
+	int i;
+	string closed = "- - -", open = "* * *";
+	int colour[] = { RED, GREEN, BLUE };
+
+	cout << closed;
+
+	for (i = 0; i < 3; i++) {
+		Sleep(1000);
+
+		clearScreen(5);
+		changeColour(colour[i]);
+
+		cout << open;
+
+		Sleep(1000);
+
+		clearScreen(5);
+		changeColour(WHITE);
+
+		cout << closed;
+	}
+
+	clearScreen(5);
+	changeColour(WHITE);
+}
+
+
+void showBlinkingLights() {
+	changeColour(RED);
+	cout << "CONSOLE ";
+	Sleep(1000);
+
+	changeColour(GREEN);
+	cout << "TANKS ";
+	Sleep(1000);
+
+	changeColour(BLUE);
+	cout << " C++";
+	Sleep(1000);
+
+	changeColour(WHITE);
+}
+
 /* Main Function */
 int main() {
+
+	cout << "\n\n\n";
+	cout << "      ";
+
+	showLoadingScreen();
+	showBlinkingLights();
+
+	cout << "\n\n\n";
 	/* Give a value to all integer variables we declared */
 	x = 6;
 	y = 6;
